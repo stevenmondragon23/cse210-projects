4f93f93bad512a579d7fcb9ac3be5f90724eb366
stevenmondragon23 <smondragonflores@byupathway.edu>
2025-09-10 12:03:33 -0500

    1009

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2000;
+        job1._endYear = 2002;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Apple";
+        job2._startYear = 2000;
+        job2._endYear = 2002;
+
+        Console.WriteLine(job1._company + "-" + job2._company);
+
+
+
+
     }
 }
